9881c5d9e7c5f5d57df3891100678cf66ef8f26e
timkleier <tim.kleier@elic.org>
2013-01-25 17:21:12 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Profiles::Application.config.secret_token = '0dd1dff3f69c3f1c03d246065db77e94e31a5a3c4a34ccd7ff2861d8a04064609116ed0d658ac4d41733d03aaf338c444bc9ebbaf32df8b4f91743c45c46d1b8'
